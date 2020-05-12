23196a09cb2489eb6bd9e4097cac900d8e143a7b
Danh Nhan Phung <danhnhan.phung@gmail.com>
2020-05-12 22:01:51 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+require("./services/passport");
 const app = express();
+
+require("./routes/authRoutes")(app);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
